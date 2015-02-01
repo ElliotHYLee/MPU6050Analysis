8999b05a0853f15924d4de8bcd0aa2b639075f29
ElliotHYLee <hylee101001@gmail.com>
2015-01-31 21:40:01 -0500

    dd
    dd

---
+++ MPU6050DataCollector/MPU6050DataCollector/Data/Day02/practice.m
@@ -0,0 +1,34 @@
+clc, clear, close all;
+
+filename = 'data01.xlsx';
+
+
+tableCFilter = xlsread(filename, 'CFilter');
+cFilter_xRaw = tableCFilter(:,1);
+
+tableAccelerometer = xlsread(filename, 'Accelerometer');
+acc_xRaw = tableAccelerometer(:,1);
+
+tableGyro = xlsread(filename, 'Gyroscope');
+gyro_xRaw = tableGyro(:,1);
+
+figure(1)
+x=1:1:length(cFilter_xRaw);
+plot(x, cFilter_xRaw, '-r')
+hold on
+
+x=1:1:length(gyro_xRaw);
+plot(x, gyro_xRaw*10, '-g')
+
+x=1:1:length(acc_xRaw);
+plot(x, acc_xRaw, '-b')
+
+grid on
+
+legend('compFilter', 'gyro*10', 'acc')
+
+
+
+
+
+
