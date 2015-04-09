11671004dd2eb381dead0f4867bcd929c3f9cf40
ElliotHYLee <hylee101001@gmail.com>
2015-04-09 13:12:18 -0400

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	MPU6050DataCollector/MPU6050DataCollector.v11.suo
    
    	MPU6050DataCollector/MPU6050DataCollector/obj/Release/DesignTimeResolveAssemblyReferencesInput.cache
    
    	MPU6050DataCollector/MPU6050DataCollector/obj/Release/MPU6050DataCollector_MarkupCompile.i.cache

---
+++ MPU6050DataCollector/MPU6050DataCollector/Data/PID_Day01/practice.m
@@ -0,0 +1,35 @@
+clc, clear, close all;
+
+filename = 'data07.xlsx';
+
+
+tableCFilter = xlsread(filename, 'CFilter');
+cFilter_xRaw = tableCFilter(:,1);
+
+tableAccelerometer = xlsread(filename, 'Accelerometer');
+acc_xRaw = tableAccelerometer(:,1);
+
+tableGyro = xlsread(filename, 'Gyroscope');
+gyro_xRaw = tableGyro(:,1);
+
+figure(1)
+x=1:1:length(cFilter_xRaw);
+plot(x, cFilter_xRaw/8234*90, '-or')
+hold on
+
+% x=1:1:length(gyro_xRaw);
+% plot(x, gyro_xRaw, '-xg')
+% 
+% x=1:1:length(acc_xRaw);
+% plot(x, acc_xRaw, '-b')
+
+grid on
+hold off
+legend('compFilter', 'gyro integral', 'acc')
+ylim([-10 10])
+xlabel('iteration')
+ylabel('degree')
+
+mean(cFilter_xRaw/8234*90)
+
+
